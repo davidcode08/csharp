90d541b6ec7b6b990cbcc4af77d475d571b402f1
daviidvo <voquochung.hcm1995@gmail.com>
2015-10-09 12:21:51 +0700

    First commit

---
+++ shoppingcart/Assign1Project/Assign1Project/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign1Project
+{
+    class Program
+    {
+        // Class objects
+        Import_Update importExporter = new Import_Update();
+        UIDisplay UIComponent = new UIDisplay();
+        ExceptionMessage exception = new ExceptionMessage();
+        Finder finder = new Finder();
+
+        static void Main(string[] args)
+        {
+            // Object declaration
+            Program p = new Program();
+
+            // Synchronize object field references
+            p.UIComponent.modifiedInventory = p.importExporter.inventory;
+            p.UIComponent.finder = p.finder;
+            p.UIComponent.exception = p.exception;
+            p.UIComponent.importer = p.importExporter;
+            p.finder.modifiedInventory = p.UIComponent.modifiedInventory;
+            p.finder.shopperCart = p.UIComponent.shopperCart;
+
+            // Display UI
+            p.UIComponent.beginDisplay();
+            p.UIComponent.displayShop();
+        }
+
+    }
+}
